cab0b8cff8b9aea9ea9bc9a8b9951e9202aae94f
AgustinLarrubia <agustinlarrubia@gmail.com>
2026-04-09 02:59:05 +0000

    Se implemento algoritmo BFS

--- tp-pathfinding/src/pathfinder/search/bfs.py
+++ tp-pathfinding/src/pathfinder/search/bfs.py
@@ -22,8 +22,31 @@ class BreadthFirstSearch:
         reached = {}
         reached[root.state] = True
 
-        # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
+        if(grid.objective_test(root.state)):
+            return Solution(root, reached)
 
-        return NoSolution(reached)
+        # Initialize frontier with the root node
+        frontera = QueueFrontier()
+
+        frontera.add(root)
+
+        while True:
+            
+            if frontera.is_empty():    
+                return NoSolution(reached)
+            
+            n = frontera.remove()
+            
+            for a in grid.actions(n.state):
+
+                s = grid.result(n.state, a)
+
+                if s not in reached:
+                    n2 = Node("", s, n.cost + grid.individual_cost(n.state,a), n, a)
+
+                    if grid.objective_test(s):
+                        return Solution(n2, reached)
+                    
+                    reached[s] = True
+        
+                    frontera.add(n2)
