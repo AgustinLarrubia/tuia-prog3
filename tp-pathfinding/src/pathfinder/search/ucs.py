42110ea9001f58562f8302001b24bb11c261791b
AgustinLarrubia <agustinlarrubia@gmail.com>
2026-04-15 02:21:52 +0000

    Se mergeo ucs desde Bonnie

--- tp-pathfinding/src/pathfinder/search/ucs.py
+++ tp-pathfinding/src/pathfinder/search/ucs.py
@@ -18,12 +18,41 @@ class UniformCostSearch:
         # Initialize root node
         root = Node("", state=grid.initial, cost=0, parent=None, action=None)
 
-        # Initialize reached with the initial state
+        # Initialize frontier with the root node
+        frontera = PriorityQueueFrontier()
+        frontera.add(root, root.cost)
+
+         # Initialize reached with the initial state
         reached = {}
         reached[root.state] = root.cost
 
-        # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
+        while True:
 
-        return NoSolution(reached)
+            if frontera.is_empty():
+                return NoSolution(reached)
+            
+            n = frontera.pop()
+
+            if grid.objective_test(n.state):
+                return Solution(n, reached)
+            
+            # Expande el nodo actual
+            for a in grid.actions(n.state):
+
+                s = grid.result(n.state, a)
+
+                c = n.cost + grid.individual_cost(n.state, a)
+
+                if s not in reached or c < reached[s]:
+
+                    # Crea un nuevo nodo
+                    n2= Node("", s, c, n, a) 
+
+                    #Actualiza el diccionario de estados alcanzados
+                    reached[s] = c 
+
+                    # Añade el nuevo nodo a la frontera con su costo como prioridad
+                    frontera.add(n2, c)
+                
+        
+    
